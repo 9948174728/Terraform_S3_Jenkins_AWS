c68913c46fcfbeee134bf5cac661a6c817c798f2
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-10-22 21:01:25 +0530

    commit message

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 #   region = "us-east-1"
 # }
 
-resource "aws_s3_bucket" "jenkins-project-s3-bucket" {
+resource "aws_s3_bucket" "jenkinsprojectbucket" {
   bucket = "jenkins-project-s3-bucket"
   acl = "private"
 }
