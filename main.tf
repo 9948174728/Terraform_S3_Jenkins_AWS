9c8e963a84eae6d30c82ccf8de449fbcb9241b9b
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-10-22 21:09:01 +0530

    commt

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@
 #   region = "us-east-1"
 # }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "jenkinsprojectbucket" {
   bucket = "jenkins-project-s3-bucket"
   acl = "private"
