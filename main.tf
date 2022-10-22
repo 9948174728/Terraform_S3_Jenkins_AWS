943febd1b1b0fec165bcc2e44c32ace0f93ea580
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-10-22 19:58:28 +0530

    profile

--- main.tf
+++ main.tf
@@ -4,7 +4,10 @@
 #   region = "us-east-1"
 # }
 
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
 
 resource "aws_s3_bucket" "jenkins-project-s3-bucket" {
   bucket = "jenkins-project-s3-bucket"
