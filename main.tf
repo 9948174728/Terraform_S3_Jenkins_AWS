521f3acb42167593cc89da4246276137267d29b3
9948174728 <137755491+9948174728@users.noreply.github.com>
2023-08-22 16:51:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "Terraform-S3_bucket" {
+resource "aws_s3_bucket" "Terra-S3_bucket" {
   bucket = "jenkins-project-s3-bucket"
   acl = "private"
 }
